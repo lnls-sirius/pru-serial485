9e7540b29cfff46002322f81be9b439c4d7dbe2d
Patricia Nallin <patricia.nallin@lnls.br>
2017-04-28 14:51:25 -0300

    New organizing folders and  Python library is now installed on system

--- src/library/overlay_build.sh
+++ src/library/overlay_build.sh
@@ -3,8 +3,6 @@
 export SLOTS=/sys/devices/bone_capemgr.9/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 
-echo "Compilando Device Tree Overlay..."
-
 dtc -O dtb -o PRUserial485-00A0.dtbo -b 0 -@ PRUserial485-00A0.dts
 dtc -O dtb -o ADDRserial485-00A0.dtbo  -b 0 -@ ADDRserial485-00A0.dts
 
--- src/library_build.sh
+++ src/library_build.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env sh
+cd library
+
+echo "."
+echo ".."
+echo "..."
+echo "Building Device Tree Overlay..."
+./overlay_build.sh
+echo "OK"
+
+echo "."
+echo ".."
+echo "..."
+echo "Building and installing C library..."
 gcc -mfloat-abi=hard -Wall -fPIC -O2 -mtune=cortex-a8 -march=armv7-a -I/usr/include -c -o PRUserial485.o PRUserial485.c
 ar -rv libPRUserial485.a PRUserial485.o
 gcc -shared -Wl,-soname, -o libPRUserial485.so PRUserial485.o
 pasm -V3 -b PRUserial485.p
-
 
 install -m0755 libPRUserial485.a libPRUserial485.so /usr/lib
 ldconfig -n /usr/lib/libPRUserial485.*
 install -m0755 PRUserial485.h /usr/include
 
 mv PRUserial485.bin /usr/bin
+rm PRUserial485.o libPRUserial485.so libPRUserial485.a
+echo "OK"
 
-rm PRUserial485.o libPRUserial485.so libPRUserial485.a 
+
+echo "."
+echo ".."
+echo "..."
+echo "Building and installing Python library..."
+python setup.py install
+echo "OK"
+
+echo "."
+echo ".."
+echo "..."
+echo "Done!"
+
+cd ..
